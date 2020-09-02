672904ccfa4bce89836e094894a64d2fcfc20b5e
Natal Vande Casteele <natal@libius.be>
2020-09-02 18:38:13 +0200

    Voorbeeldje eigen data tonen

--- README.md
+++ README.md
@@ -7,10 +7,6 @@ Gegenereerde HTML werd in index.html geplakt.
 
 ### Opstarten
 
-Genereer een self signed certificate (server.key en server.cert) via  
-
-```openssl req -nodes -new -x509 -keyout server.key -out server.cert```
-
 Dan 
 
 ```
@@ -18,7 +14,4 @@ Dan
   node index.js
 ```
    
-Ga naar https://apps-local.mow.vlaanderen.be/
-
-Start chrome op met parameter --ignore-certificate-errors om invalid certificate warning niet te krijgen.
-Bvb /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --ignore-certificate-errors
+Ga naar http://localhost/
--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var express = require('express');
-var https = require('https');
-var fs = require('fs');
-var app = express();
 
+var app = express();
 
 app.use(express.static('.'));
 app.use(express.static('node_modules/@wegenenverkeer/ng-kaart-webcomponent'));
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-}, app)
-    .listen(443, function () {
-        console.log('Ga naar https://apps-local.mow.vlaanderen.be/')
+app.listen(80, function () {
+        console.log('Ga naar http://localhost/')
     })
